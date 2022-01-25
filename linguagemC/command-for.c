15d386b5f048a2ac1e3e03f34b2c7b0269f534de
erlan carvalho <erlangleidson@hotmail.com>
2022-01-25 14:58:38 -0300

    Command for

---
+++ linguagemC/command-for.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int N ;
+    scanf("%d",&N);
+    /*
+    int i = 1; // initialization
+
+    while(N  >= i ){//condition
+    printf("Neps!\n");
+    i = i+1;//increment that changes the condition, helping to finish the code
+    }
+
+    */
+
+    int i;
+
+    for(i = 1; i<=N ; i=i+1){
+        printf("Neps\n");
+
+    }
+
+}
