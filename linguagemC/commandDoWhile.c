94375ac7e6c6fdc1a10d8db83974e121a4e7f22a
erlan carvalho <erlangleidson@hotmail.com>
2022-01-27 21:02:42 -0300

    Command do while

---
+++ linguagemC/commandDoWhile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int N;
+
+    /*while(N!=2018){
+    scanf("%d",&N);
+    }
+    */
+
+    int i ;
+    for(i = 2018; N!=i;){
+    scanf("%d", &N);
+
+    }
+
+    /*do{
+    scanf("%d", &N);
+    }while(N!=2018);
+
+     */
+
+}
