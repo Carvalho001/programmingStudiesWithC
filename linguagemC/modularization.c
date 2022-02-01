80e3e02e70842b857eaa9ee8984de5e97adced5d
erlan carvalho <erlangleidson@hotmail.com>
2022-02-01 00:30:42 -0300

    Conceito de modularizacao

---
+++ linguagemC/modularization.c
@@ -0,0 +1,25 @@
+
+/*A funtcion sintax in C it like below
+
+    <return type> <name> (parameters){
+        <code>
+    }
+
+*/
+
+//example of a functiion(without parameters)
+
+#include <stdio.h>
+int prints(){
+    printf("hello Neps Academy\n");
+
+}
+
+int main(){
+
+    //here we can summon up(or call) the function
+    prints();
+    prints();
+
+
+}
